afc21732fd8112612aaf95504906ef4dd6cd3e7e
Jonks <anastasiablackhold@gmail.com>
2021-02-01 23:12:38 +0000

    New assertEqual.js file

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion Failed: ${actual} !== ${expected} 😩️`);
+  }
+
+};
+
+assertEqual("lighthouse Labs", "bootcamp");
+assertEqual(1, 1);
