5455f4b1c5c5d92bc6db14f41d98e98cd9b72f1d
Jonks <anastasiablackhold@gmail.com>
2021-02-02 00:29:56 +0000

    New head.js file - constains assertEqual function

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion Failed: ${actual} !== ${expected} 😩️`);
+  }
+
+};
+
+const head = function(array) {
+  return array[0];
+
+};
+
