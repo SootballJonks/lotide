016791773ee9c2b8858787d1ad092a14a9e984b5
Jonks <anastasiablackhold@gmail.com>
2021-02-04 05:00:09 +0000

    assertEqual function but for objects!

---
+++ assertObjectsEqual.js
@@ -0,0 +1,46 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = i; j <= i; j++) {
+      if (array1[i] !== array2[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  } 
+  return true;
+};
+
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2)) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${inspect(object1)} === ${inspect(object2)} ✨️✨️`);
+  } else {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${inspect(object1)} !== ${inspect(object2)} 😩️`);
+  }
+};
+
+const cd = { d: ['2', 3], c: '1' };
+const dc = { c: '1', d: ['2', 3] };
+
+assertObjectsEqual(cd, dc);
